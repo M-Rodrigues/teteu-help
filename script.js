5869af6366c51fba26e09cc1517dbffa5c16d92a
M-Rodrigues <matheus.rodrigues.ime@gmail.com>
2021-01-19 21:55:38 -0200

    Requisito 1

---
+++ script.js
@@ -0,0 +1,62 @@
+async function fetchAndRenderizeProducts() {
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const { results } = await response.json();
+    console.log(results);
+    results.forEach((product) => {
+      const { thumbnail, id, title } = product;
+
+      const productItem = createProductItemElement({ sku: id, name: title, image: thumbnail });
+      document.querySelector('.items').appendChild(productItem);
+    });
+  } catch (error) {
+    console.log('erro aqui', error);
+  }
+}
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  console.log({ sku, name, image });
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+window.onload = function onload() {
+  fetchAndRenderizeProducts();
+};
